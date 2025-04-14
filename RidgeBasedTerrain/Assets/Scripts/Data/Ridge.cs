5ccfd6ab34b1c28d9eb0b730067b2531dd1c083a
Furkan Yıldız <yildiz.furkan@hotmail.com>
2025-04-14 16:55:32 +0300

    1

---
+++ RidgeBasedTerrain/Assets/Scripts/Data/Ridge.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Ridge
+{
+    private Vector3 _start;
+    private Vector3 _end;
+    private List<Vector3> _points;
+
+    public Ridge(Vector3 start, Vector3 end)
+    {
+        _start = start;
+        _end = end;
+    }
+
+    public void SetPoints(List<Vector3> points)
+    {
+        _points = points;
+    }
+
+    public List<Vector3> GetPoints()
+    {
+        return _points;
+    }
+}
---
+++ RidgeBasedTerrain/Assets/Scripts/Data/RidgeMesh.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class RidgeMesh : TileMesh
+{
+    private List<TileMesh> _neighbours = new();
+    private List<Ridge> _ridges = new();
+
+    private float _yShift = 0f;
+    private float _yCompress = 1f;
+    
+    public RidgeMesh(Hexagon hexagon, HexMeshParams hexMeshParams) : base(hexagon, hexMeshParams)
+    {
+    }
+}
